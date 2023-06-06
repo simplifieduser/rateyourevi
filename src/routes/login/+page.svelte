ffc363838eb5841cf01e4d085a416baa1621eda8
Jonah Zabel <develop.polony@gmx.net>
2023-06-06 16:15:40 +0200

    Added auth0 redirect to login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,45 +1,20 @@
 <script lang="ts">
-  import { goto } from "$app/navigation";
-  import { page } from "$app/stores";
+  import { WebAuth } from "auth0-js";
   import { onMount } from "svelte";
-  import type { ActionData } from "./$types";
-
-  export let form: ActionData
-  let redirect = $page.url.searchParams.get("r") || ""
 
   onMount(() => {
-    if (form && form.success) {
-      goto(form.data.redirect)
-    }
+    const webAuth = new WebAuth({
+        domain: "simplifieduser.eu.auth0.com",
+        clientID: "2cOKd4LLb1P13vnU9P1omiTHvBkSyEFE",
+        responseType: "token",
+        redirectUri: "https://localhost:5173/",
+    })
+
+    webAuth.authorize()
   })
 
 </script>
 
 <main>
 
-  <form method="post">
-    <input id="c" name="c" type="text" placeholder="Zugangscode">
-    <input bind:value={redirect} id="r" class="hidden" name="r" type="text">
-    <button>Weiter</button>
-  </form>
-
-  {#if form && !form.success}
-
-    {#if form.error.reason === "unauthorized"}
-      Der eingegebene Zugangscode ist ungültig! Bitte versuche es erneut.
-
-    {:else if form.error.reason === "missing"}
-      Bitte gebe deinen Zugangscode ein.
-
-    {/if}
-
-  {/if}
-
-
 </main>
-
-<style>
-  .hidden {
-    display: none;
-  }
-</style>
