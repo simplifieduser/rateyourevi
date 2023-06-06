b1cf7968a5fb56f3bee0065f5fceee5bf7d95a72
Jonah Zabel <develop.polony@gmx.net>
2023-06-06 16:16:08 +0200

    Added rating list view

---
+++ src/routes/rating/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  export let data: PageData
+
+</script>
+
+<main>
+  {#if data.success}
+    <ol>
+      {#each data.data.students as student}
+        <li>{student.fullName}</li>
+      {/each}
+    </ol>
+
+  {:else}
+    Es ist ein Fehler aufgetreten!
+  {/if}
+</main>
