7cf9cab81d83d5a0202f3691848d5a6342169942
Jonah Zabel <develop.polony@gmx.net>
2023-06-09 12:25:16 +0200

    Added login logic

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,20 +1,44 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
+  import { page } from "$app/stores";
   import { WebAuth } from "auth0-js";
   import { onMount } from "svelte";
 
+
   onMount(() => {
-    const webAuth = new WebAuth({
-        domain: "simplifieduser.eu.auth0.com",
-        clientID: "2cOKd4LLb1P13vnU9P1omiTHvBkSyEFE",
-        responseType: "token",
-        redirectUri: "https://localhost:5173/",
+
+    const client = new WebAuth({
+      domain: "simplifieduser.eu.auth0.com",
+      clientID: "LlKXTOza5PVGlWoe2EVqrmv207WwTZTp",
     })
 
-    webAuth.authorize()
+    client.parseHash((error, result) => {
+
+      const redirectUrl = $page.url.searchParams.get("r") || "/"
+
+      if (document.cookie.includes("token")) {
+        goto(redirectUrl)
+        return
+      }
+
+      if (error || !result) {
+        client.authorize({ 
+          redirectUri: `http://localhost:5173/login/r=${redirectUrl}`,
+          responseType: "token" 
+        })
+        return
+      }
+
+      document.cookie = `token=${result.accessToken}; expires=${result.expiresIn}; path="/"`
+
+      goto(redirectUrl)  
+
+    })
+
   })
 
 </script>
 
 <main>
-
+  Redirecting...
 </main>
