d3afe3c01c0134c328cee21553a47593e33d7d16
Jonah Zabel <develop.polony@gmx.net>
2023-06-09 12:25:28 +0200

    Added logout route & logic

---
+++ src/routes/logout/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { WebAuth } from "auth0-js";
+  import { onMount } from "svelte";deprecated
+
+  onMount(() => {
+
+    const client = new WebAuth({
+      domain: "simplifieduser.eu.auth0.com",
+      clientID: "LlKXTOza5PVGlWoe2EVqrmv207WwTZTp",
+    })
+
+    const redirectUrl = $page.url.searchParams.get("r") || "/"
+    document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+
+    client.logout({
+      returnTo: redirectUrl
+    })
+
+  })
+
+</script>
+
+<main>
+  Redirecting...
+</main>
