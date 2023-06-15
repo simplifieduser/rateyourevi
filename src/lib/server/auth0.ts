5f15a018ce89fc4f1c9f614bfa926153da4b02b0
SimplifiedUser <develop.polony@gmx.net>
2023-06-15 21:03:43 +0200

    Fixing issue with auth0 api

--- src/lib/server/auth0.ts
+++ src/lib/server/auth0.ts
@@ -1,6 +1,6 @@
-import { AuthenticationClient } from "auth0";
+import pkg from "auth0";
 
-const auth0 = new AuthenticationClient({
+const auth0 = new pkg.AuthenticationClient({
   domain: "simplifieduser.eu.auth0.com",
   clientId: "LlKXTOza5PVGlWoe2EVqrmv207WwTZTp",
 })
